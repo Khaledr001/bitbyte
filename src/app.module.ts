2750fd2e0467ac7a1e4cb409e192bd80b49369fa
Khaledr001 <khaledrazu01@gmail.com>
2024-11-08 00:41:00 +0600

    database module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { APP_CONFIG } from './config/app.config';
-import { DATABASE_CONFIG } from './config/database.config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [APP_CONFIG, DATABASE_CONFIG],
+      load: [APP_CONFIG],
       cache: true,
       expandVariables: true,
       isGlobal: true,
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
